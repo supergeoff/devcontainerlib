83b214153d79319158807ff3c65c398d8566e635
Geoff <geoffroy.lindas@gmail.com>
2025-05-22 14:43:29 +0200

    Update .zshrc.sh script URL in setup.sh for multiple environments

--- nx-full/.devcontainer/setup.sh
+++ nx-full/.devcontainer/setup.sh
@@ -2,5 +2,5 @@
 
 curl -L git.io/antigen > ~/.antigen.zsh
 
-curl -fsSL https://gist.githubusercontent.com/supergeoff/0beee31eb794c5ef9780a7a143d2ccda/raw/3cdb4a41d784f62d8ae45bbd0ca2fb4dcdf9a225/.zshrc.sh
+curl -fsSL https://gist.githubusercontent.com/supergeoff/0beee31eb794c5ef9780a7a143d2ccda/raw/d7fe200e2ec8d38f8de7ce5606e8ebee1a283abf/.zshrc.sh
 mv ~/.zshrc.sh ~/.zshrc
