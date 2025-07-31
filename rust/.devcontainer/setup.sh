cb8f41771658c41caee01831f489d65dae34d092
Geoff <geoffroy.lindas@gmail.com>
2025-07-31 16:08:43 +0200

    Add initial devcontainer configuration and setup script for Rust environment

---
+++ rust/.devcontainer/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -L git.io/antigen > ~/.antigen.zsh
+
+curl -fsSL https://gist.githubusercontent.com/supergeoff/0beee31eb794c5ef9780a7a143d2ccda/raw/7300698a15611dabc91f8d2bc07367b09c103971/.zshrc.sh -o ~/.zshrc.sh
+mv ~/.zshrc.sh ~/.zshrc
